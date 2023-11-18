82476a59f91214a36bfb630d983e53ffd5ce1039
Mark Krautheim <mark.krautheim@gmail.com>
2023-11-18 11:03:58 -0500

    Add check for active Python venv

--- bin/python.sh
+++ bin/python.sh
@@ -1,5 +1,9 @@
-source $VIRTUAL_ENV/../config/env_defaults.sh
-if [ -f $VIRTUAL_ENV/../config/env_local.sh ]; then
-  source $VIRTUAL_ENV/../config/env_local.sh
+if [[ ! -v VIRTUAL_ENV ]]; then
+  echo "You must be in an active Python virtual environment!"
+else
+  source $VIRTUAL_ENV/../config/env_defaults.sh
+  if [ -f $VIRTUAL_ENV/../config/env_local.sh ]; then
+    source $VIRTUAL_ENV/../config/env_local.sh
+  fi
+  python "$@"
 fi
-python "$@"
